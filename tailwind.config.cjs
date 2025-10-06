5179f79f2bdf263482121e278c3fb2356a6e9014
metaxuae <media@metaxuae.com>
2025-10-06 17:18:43 +0400

    updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",                 // ← add this line
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
